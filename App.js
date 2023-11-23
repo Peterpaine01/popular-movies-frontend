57aeb48d17bd9e73f68ed09b8233e7b8036985c2
Fanny Carlier <fannycarlier@mbp-de-fanny.home>
2023-11-23 15:27:06 +0100

    key

--- App.js
+++ App.js
@@ -12,44 +12,44 @@ import MovieScreen from "./containers/MovieScreen";
 const Stack = createNativeStackNavigator();
 const ApiKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGFjMTk3MjNhNzg4NTAwMTQzODg3YTYiLCJlbWFpbCI6ImZhY2FybGllckBnbWFpbC5jb20iLCJleHBpcmF0aW9uRGF0ZSI6IjIwMjQtMDMtMDhUMDA6MDA6MDAuMDAwWiIsImlzVHJhaW5pbmciOnRydWUsImlhdCI6MTcwMDc0NzA4OH0.gLcRmwgsWOAuztEhLM8QEZrX0x-odQvZIHY9KdBq5oA";
-await AsyncStorage.setItem("userToken", ApiKey);
+// await AsyncStorage.setItem("userToken", ApiKey);
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [userToken, setUserToken] = useState(null);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [userToken, setUserToken] = useState(null);
 
-  // const setToken = async (token) => {
-  //   if (token) {
-  //     // Connexion
-  //     await AsyncStorage.setItem("userToken", token);
-  //   } else {
-  //     // Deconnexion
-  //     await AsyncStorage.removeItem("userToken");
-  //   }
+  // // const setToken = async (token) => {
+  // //   if (token) {
+  // //     // Connexion
+  // //     await AsyncStorage.setItem("userToken", token);
+  // //   } else {
+  // //     // Deconnexion
+  // //     await AsyncStorage.removeItem("userToken");
+  // //   }
 
-  //   setUserToken(token);
-  // };
+  // //   setUserToken(token);
+  // // };
 
-  useEffect(() => {
-    // Fetch the token from storage then navigate to our appropriate place
-    const bootstrapAsync = async () => {
-      // We should also handle error for production apps
-      const userToken = await AsyncStorage.getItem("userToken");
+  // useEffect(() => {
+  //   // Fetch the token from storage then navigate to our appropriate place
+  //   const bootstrapAsync = async () => {
+  //     // We should also handle error for production apps
+  //     const userToken = await AsyncStorage.getItem("userToken");
 
-      // This will switch to the App screen or Auth screen and this loading
-      // screen will be unmounted and thrown away.
-      setUserToken(userToken);
-      console.log();
-      setIsLoading(false);
-    };
+  //     // This will switch to the App screen or Auth screen and this loading
+  //     // screen will be unmounted and thrown away.
+  //     setUserToken(userToken);
+  //     console.log();
+  //     setIsLoading(false);
+  //   };
 
-    bootstrapAsync();
-  }, []);
+  //   bootstrapAsync();
+  // }, []);
 
-  if (isLoading === true) {
-    // We haven't finished checking for the token yet
-    return <ActivityIndicator />;
-  }
+  // if (isLoading === true) {
+  //   // We haven't finished checking for the token yet
+  //   return <ActivityIndicator />;
+  // }
 
   return (
     <NavigationContainer>
